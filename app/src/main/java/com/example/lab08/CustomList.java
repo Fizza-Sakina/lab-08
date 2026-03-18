63607e8bb592c62fc28eb6a51e07c4204db9e446
Fizza-Sakina <27100391@lums.edu.pk>
2026-03-18 21:52:05 +0500

    add lab8

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -14,7 +14,8 @@ public class CustomList {
         cities.add(city);
     }
 
-    
+
+
 
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
