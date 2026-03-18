189069c78053ecd586fa4102fa27f8cc380f2739
Fizza-Sakina <27100391@lums.edu.pk>
2026-03-18 22:01:23 +0500

    addlab8

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -15,7 +15,6 @@ public class CustomList {
     }
 
 
-
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
 }
--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -14,4 +14,5 @@ public class CustomListTest {
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+
 }
